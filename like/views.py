a69182e2c5df89266270974d6fa10897a7f636db
ruth <ruthwanjiramugo@gmail.com>
2020-05-29 08:57:41 +0300

    add the view function of the registration form

--- like/views.py
+++ like/views.py
@@ -1,11 +1,9 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from django.shortcuts import render
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 from django.http  import HttpResponse,Http404
+from django.contrib.auth.decorators import login_required
 # Create your views here.
-
+@login_required(login_url='/accounts/login/')
 def welcome(request):
     
     return render(request,'fold/welcome.html')
--- mwaa_lr/settings.py
+++ mwaa_lr/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'like.apps.LikeConfig',
+    'users.apps.UsersConfig',
     'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
