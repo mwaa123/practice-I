2dad5fa554f9a3845986525ec05002b3771e554a
ruth <ruthwanjiramugo@gmail.com>
2020-05-29 10:01:24 +0300

    add the navbar

--- like/views.py
+++ like/views.py
@@ -3,7 +3,7 @@ from django.http  import HttpResponse
 from django.http  import HttpResponse,Http404
 from django.contrib.auth.decorators import login_required
 # Create your views here.
-@login_required(login_url='/accounts/login/')
+# @login_required(login_url='/accounts/login/')
 def welcome(request):
     
     return render(request,'fold/welcome.html')
