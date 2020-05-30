fec51c26d5f03cb5be2c6e980820f40fbf8ebd3d
ruth <ruthwanjiramugo@gmail.com>
2020-05-30 17:37:51 +0300

    add the functioms to update the the profile of the user

--- like/views.py
+++ like/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
-from django.http  import HttpResponse
-from django.http  import HttpResponse,Http404
+from django.contrib import messages
+from .forms import UserCreationForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile,Image
 # Create your views here.
 # @login_required(login_url='/accounts/login/')
 def welcome(request):
@@ -10,5 +11,29 @@ def welcome(request):
 
 
 def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form':u_form,
+        'p_form':p_form
+    }
+    return render(request,'fold/profile.html',context)
+
+
+
+
+
     
-    return render(request,'fold/profile.html')
--- mwaa_lr/settings.py
+++ mwaa_lr/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'like.apps.LikeConfig',
     'users.apps.UsersConfig',
     'bootstrap3',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
