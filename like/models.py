5d83296da2f2b96b34394f04a6b3ad02948d8167
ruth <ruthwanjiramugo@gmail.com>
2020-05-30 08:18:21 +0300

    add the image model

--- like/models.py
+++ like/models.py
@@ -5,9 +5,21 @@ from django.contrib.auth.models import User
 class Profile (models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     bio = models.TextField()
-    image = models.ImageField(default="default.jpg",upload_to='gala/')
+    img= models.ImageField(default="default.jpg",upload_to='gala/')
     name = models.TextField( blank=True)
 
       
     def __str__(self):
         return f'{self.user.username} Profile'
+
+
+
+class Image(models.Model):
+    name = models.TextField()
+    caption = models.TextField()
+    img= models.ImageField(upload_to='gala/',blank=True)
+    comments = models.TextField()
+    prof= models.ForeignKey('Profile',on_delete=models.CASCADE ,null=True )
+
+    def __str__(self):
+        return f'{self.user.username}name'                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
